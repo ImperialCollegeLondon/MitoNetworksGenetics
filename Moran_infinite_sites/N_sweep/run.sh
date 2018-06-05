7ae8af61cad1d128627de704fd29da8624960373
Juvid <ja1109@ic.ac.uk>
2018-06-05 14:12:36 +0100

    Clean up and rename

--- Moran_infinite_sites/N_sweep/run.sh
+++ Moran_infinite_sites/N_sweep/run.sh
@@ -2,9 +2,9 @@
 GSL_HOME="/home/juvid/gsl"
 LD_LIBRARY_PATH="/home/juvid/gsl/lib"
 export LD_LIBRARY_PATH
-gcc -Wall -std=gnu99 -o infinite_alleles_mtDNA_N_sweep.ce  infinite_alleles_mtDNA_N_sweep.c -I$GSL_HOME/include -L$GSL_HOME/lib -lgsl -lgslcblas -lm
-./infinite_alleles_mtDNA_N_sweep.ce 413032 0.023 5.6e-7 100
-./infinite_alleles_mtDNA_N_sweep.ce 218360 0.023 5.6e-7 500
-./infinite_alleles_mtDNA_N_sweep.ce 270161 0.023 5.6e-7 1000
-./infinite_alleles_mtDNA_N_sweep.ce 79778 0.023 5.6e-7 1500
-./infinite_alleles_mtDNA_N_sweep.ce 814137 0.023 5.6e-7 2000
+gcc -Wall -std=gnu99 -o infinite_sites_mtDNA_N_sweep.ce  infinite_sites_mtDNA_N_sweep.c -I$GSL_HOME/include -L$GSL_HOME/lib -lgsl -lgslcblas -lm
+./infinite_sites_mtDNA_N_sweep.ce 413032 0.023 5.6e-7 100
+./infinite_sites_mtDNA_N_sweep.ce 218360 0.023 5.6e-7 500
+./infinite_sites_mtDNA_N_sweep.ce 270161 0.023 5.6e-7 1000
+./infinite_sites_mtDNA_N_sweep.ce 79778 0.023 5.6e-7 1500
+./infinite_sites_mtDNA_N_sweep.ce 814137 0.023 5.6e-7 2000
