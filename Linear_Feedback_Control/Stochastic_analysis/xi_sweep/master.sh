587a9b085192790674385d5b8db301a87e9ceb0e
Juvid <ja1109@ic.ac.uk>
2018-07-05 13:21:20 +0100

    xi sweep

---
+++ Linear_Feedback_Control/Stochastic_analysis/xi_sweep/master.sh
@@ -0,0 +1,17 @@
+gcc -o3 closed_loop_deg_single.c -lm -o closed_loop_deg_single.ce
+echo "0"
+mkdir -p $WORK/networks/sing_birth_fus/xi_sw/p0
+qsub submission_0.pbs
+sleep 0.5
+echo "1"
+mkdir -p $WORK/networks/sing_birth_fus/xi_sw/p1
+qsub submission_1.pbs
+sleep 0.5
+echo "2"
+mkdir -p $WORK/networks/sing_birth_fus/xi_sw/p2
+qsub submission_2.pbs
+sleep 0.5
+echo "3"
+mkdir -p $WORK/networks/sing_birth_fus/xi_sw/p3
+qsub submission_3.pbs
+sleep 0.5
